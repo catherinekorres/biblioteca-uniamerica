71af27fc67f0bb7f0edc798137445ae9bc1b8d77
Catherine <catherine@catherinekorres.dev>
2020-06-21 15:09:05 -0300

    create Getters & Setters

--- src/main/java/com/uniamerica/biblioteca/model/Book.java
+++ src/main/java/com/uniamerica/biblioteca/model/Book.java
@@ -18,4 +18,36 @@ public class Book {
 
     @ManyToMany
     private List<Author> authors;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
 }
